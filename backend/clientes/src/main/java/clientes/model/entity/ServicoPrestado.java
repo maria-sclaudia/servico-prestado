0f4e0f8af0d473092ac14a66b249c01043c6bb28
Maria Claudia <mariaclaudia.dsouza@gmail.com>
2021-07-17 18:17:54 -0300

    Initial commit

---
+++ backend/clientes/src/main/java/clientes/model/entity/ServicoPrestado.java
@@ -0,0 +1,33 @@
+package clientes.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class ServicoPrestado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String descricao;
+
+    //Muitos servicos para um cliente
+    @ManyToOne
+    //chave estrangeira
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+
+    @Column
+    private BigDecimal valor;
+
+    @Column
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate data;
+}
---
+++ backend/clientes/src/main/java/clientes/util/BigDecimalConverter.java
@@ -0,0 +1,19 @@
+package clientes.util;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BigDecimalConverter {
+    private char target;
+
+    public BigDecimal converter(String value) {
+        if(value == null){
+            return null;
+        }
+
+        value = value.replace(".", "").replace(",", ".");
+        return new BigDecimal(value);
+    }
+}
